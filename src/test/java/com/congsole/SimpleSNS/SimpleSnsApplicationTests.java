ecd68d6c5a055317af2da0802633a08ba60c400b
congsole <qax741@gmail.com>
2024-03-15 16:24:56 +0900

    init : initialize project

---
+++ src/test/java/com/congsole/SimpleSNS/SimpleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congsole.SimpleSNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
